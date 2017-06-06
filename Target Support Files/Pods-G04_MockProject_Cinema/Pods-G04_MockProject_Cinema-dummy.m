f1ad97fca0acdecfc676578b1cefc8d3ceac54fe
THANH <thanh@THANHs-Mac.local>
2017-06-06 23:17:42 +0700

    Initial Commit

---
+++ Target Support Files/Pods-G04_MockProject_Cinema/Pods-G04_MockProject_Cinema-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_G04_MockProject_Cinema : NSObject
+@end
+@implementation PodsDummy_Pods_G04_MockProject_Cinema
+@end
---
+++ Target Support Files/Pods-G04_MockProject_CinemaUITests/Pods-G04_MockProject_CinemaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_G04_MockProject_CinemaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_G04_MockProject_CinemaUITests
+@end
